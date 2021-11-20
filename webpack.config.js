da481176120ffa3b458583864b6b37e059819c19
Shubham <shubham.0638@gmail.com>
2021-11-20 12:42:10 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+
+    // Specify the path to entry file here. This is where webpack begins its job
+    entry: './src/javascript/index.js',
+
+    //Path and file name of result bundle
+    // All JS will be bundled in this file
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '',
+        filename: 'bundle.js'
+    },
+
+    // Default mode is production
+    // Production bundle is minified as compared to development bundle
+    mode: 'development'
+}
